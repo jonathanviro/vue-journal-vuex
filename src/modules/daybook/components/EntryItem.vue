1a4d817b8339cff5344d24f86110f658015b7634
JonViro <105942530+jonathanviro@users.noreply.github.com>
2024-02-28 18:08:24 -0500

    First commit

---
+++ src/modules/daybook/components/EntryItem.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="entry-container mb-3 pointer p-2" 
+        @click="$router.push({ name:'entry', params: { id: 10 } })">
+        <div class="entry-title d-flex">
+            <span class="text-succes fs-5 fw-bold">28</span>
+            <span class="mx-1 fs-5">02</span>
+            <span class="mx-2 fw-ligth">2024, Miercoles</span>
+        </div>
+        <div class="entry-description">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum molestias ipsum sit dolorum iure fugit iusto hic beatae, numquam aliquid asperiores nam unde delectus iste doloribus laboriosam, rerum dolorem mollitia.
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style lang="scss" scoped>
+.entry-container {
+    border-bottom: 1px solid #2c3e50;
+    transition: 0.2s all ease-in;
+    
+    &::hover {
+        background-color: lighten($color: grey, $amount: 45);
+        transition: 0.2s all ease-in;
+    }
+
+    .entry-description {
+        font-size: 12px;
+    }
+}
+</style>
---
+++ src/modules/daybook/store/journal/state.js
@@ -0,0 +1,24 @@
+export default () => ({
+    isLoading: true,
+    entries: [
+        {
+            id: new Date().getTime(),
+            date: new Date().toDateString(),
+            text: 'Tempor anim velit laboris cupidatat excepteur. Consequat do ad eiusmod sit irure dolore voluptate. Nostrud proident aliquip laborum ullamco non deserunt ex. Fugiat nulla commodo nostrud est exercitation enim veniam ea.',
+            picture: null,
+        },
+        {
+            id: new Date().getTime() + 1000,
+            date: new Date().toDateString(),
+            text: 'In deserunt tempor do enim sint pariatur excepteur magna sint proident cupidatat non. Et sit exercitation cupidatat consequat nostrud nulla do et eu. Ad sunt minim tempor reprehenderit tempor minim dolor ipsum elit ex. Enim ea minim consectetur consequat. Culpa enim excepteur quis aliqua enim exercitation elit magna est. Consectetur labore id qui do est laborum pariatur elit non adipisicing.',
+            picture: null,
+        },
+        {
+            id: new Date().getTime() + 2000,
+            date: new Date().toDateString(),
+            text: 'Commodo adipisicing aliquip sint aliqua consequat esse quis id cillum sint id reprehenderit ea pariatur. Sint laborum sit laborum excepteur amet cupidatat nisi eu consectetur eu amet aliqua eiusmod. Veniam velit laborum eiusmod exercitation fugiat. Reprehenderit officia aliquip deserunt occaecat excepteur. Irure officia magna aliqua excepteur duis deserunt nulla cupidatat. Proident non aliquip ut fugiat adipisicing magna incididunt aute ullamco ex proident aute anim fugiat.',
+            picture: null,
+        },
+        
+    ]
+})
